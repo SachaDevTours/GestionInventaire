4d9f2434d2bbdafd6387513af6299c98b3ff5fae
SachaDevTours <sachathibault45@gmail.com>
2025-02-10 14:53:35 +0100

    Create ProductService.php

---
+++ backend/src/Services/ProductService.php
@@ -0,0 +1,26 @@
+<?php
+namespace Services;
+
+use Repositories\ProductRepository;
+use Entities\Product;
+
+class ProductService {
+    private $productRepo;
+
+    public function __construct($pdo) {
+        $this->productRepo = new ProductRepository($pdo);
+    }
+
+    public function generateQRCodeData($productId) {
+        return "/product/" . $productId; // Stocke uniquement le chemin relatif en base
+    }
+
+    public function addProduct($data) {
+        $data["qr_code"] = $this->generateQRCodeData($data["id"]);
+        return $this->productRepo->addProduct($data);
+    }
+
+    public function getAllProducts() {
+        return $this->productRepo->getAllProducts();
+    }
+}
